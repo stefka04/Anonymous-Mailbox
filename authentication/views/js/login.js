d0f6abe657442629556b20af3bcbe4e04ebf3eec
Hristina Frangova <117001358+hrisi14@users.noreply.github.com>
2025-06-13 02:17:36 +0300

    Update login.js

--- authentication/views/js/login.js
+++ authentication/views/js/login.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 function createLabel(message, messageColor) {
      const responseLabel = document.getElementById("response-label");
      responseLabel.style.display = "block";
@@ -49,56 +49,5 @@ form.addEventListener('submit', (event) => {
 const registerButton = document.getElementById("register-btn");
 registerButton.addEventListener('click', () => {
     window.location.href = "./registration-form.html";
-=======
-function createLabel(message, messageColor) {
-     const responseLabel = document.getElementById("response-label");
-     responseLabel.style.display = "block";
-     responseLabel.textContent = message;
-     responseLabel.style.color = messageColor;
-}
 
-function analyzeResponse(response, message) {
-     if (response.status === 200) {
-        createLabel(message, "green");    
-        } else {
-        createLabel(response["message"], "red");
-}
-}
-
-
-const form = document.getElementById('login-form');
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const emailField = document.getElementById("email");
-    const passwordField = document.getElementById("password");
-    const email = emailField.value.trim();
-    const password = passwordField.value.trim();
-
-    const userData = {
-        email: email,
-        password: password
-    };
-
-    fetch('../services/new-login.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userData),
-    })
-    .then(response => response.json().then(data => {
-        data.status = response.status; 
-        analyzeResponse(data, "Успешно влизане! Пренасочване към началната страница...");
-        if (data.status === 200) {
-            setTimeout(() => {
-                window.location.href = "../../inbox/index.html";
-            }, 2000);
-        }
-    }));
-});
-
-const registerButton = document.getElementById("register-btn");
-registerButton.addEventListener('click', () => {
-    window.location.href = "./registration-form.html";
->>>>>>> 16ea5ad13fa02fae007ed6d53c10d3ecb1c7b597
 });
